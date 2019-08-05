a64f5d67767b86071c8f5cfeabcda1f256c040e5
Tim <nebukam@gmail.com>
2019-08-05 02:52:43 -0400

    Updated dependencies

---
+++ Runtime/Anchor.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Mathematics;
+using static Unity.Mathematics.math;
+using Nebukam.Geom;
+
+namespace Nebukam.FFD
+{
+    public class Anchor : WorldVertex
+    {
+        
+        /// <summary>
+        /// difference between Vertex.pos & world.
+        /// </summary>
+        public float3 diff = float3(false);
+        
+    }
+}
---
+++ Runtime/AnchorGroup.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Mathematics;
+using static Unity.Mathematics.math;
+using Nebukam.Geom;
+
+namespace Nebukam.FFD
+{
+    public class AnchorGroup : WorldVertexGroup<Anchor>
+    {
+
+        protected override void OnVertexAdded(Anchor v)
+        {
+            base.OnVertexAdded(v);
+            v.diff = float3(false);
+        }
+
+    }
+}
